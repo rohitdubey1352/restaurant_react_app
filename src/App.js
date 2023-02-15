8c13ce75feb1abbcd960d69c6dcb460435933989
Rohit Dubey <56528673+rohitdubey1352@users.noreply.github.com>
2023-02-15 22:39:40 +0530

    Restaurant React App

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Restaurant from "./Component/Basics/Restaurant";
+import './Component/Basics/style.css';
+
+function App() {
+  return (
+    <>
+      <Restaurant />
+
+
+    </>
+
+  );
+}
+
+export default App;
---
+++ src/Component/Menu.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+const Menu = [
+    {
+        id: 1,
+        image: "Images/breakfast.jpg",
+        name: "Pan cake",
+        category: "Breakfast",
+        description: "lorem ipsum dolor sit amet consectetur adipisicig elit. Dolar libero cum obcaecati deserunt excepturi amet.",
+    },
+    {
+        id: 2,
+        image: "Images/lunch.jpg",
+        name: "Burger",
+        category: "Lunch",
+        description: "lorem ipsum dolor sit amet consectetur adipisicig elit. Dolar libero cum obcaecati deserunt excepturi amet.",
+    },
+    {
+        id: 3,
+        image: "Images/dinner.jpg",
+        name: "Drinks",
+        category: "Dinner",
+        description: "lorem ipsum dolor sit amet consectetur adipisicig elit. Dolar libero cum obcaecati deserunt excepturi amet.",
+    },
+    {
+        id: 4,
+        image: "Images/breakfast (2).jpg",
+        name: "Omlet",
+        category: "Breakfast",
+        description: "lorem ipsum dolor sit amet consectetur adipisicig elit. Dolar libero cum obcaecati deserunt excepturi amet.",
+    },
+    {
+        id: 5,
+        image: "Images/lunch (1).jpg",
+        name: "High Burger",
+        category: "Lunch",
+        description: "lorem ipsum dolor sit amet consectetur adipisicig elit. Dolar libero cum obcaecati deserunt excepturi amet.",
+    },
+    {
+        id: 6,
+        image: "Images/dinner (1).jpg",
+        name: "Light Dinner",
+        category: "Dinner",
+        description: "lorem ipsum dolor sit amet consectetur adipisicig elit. Dolar libero cum obcaecati deserunt excepturi amet.",
+    },
+    {
+        id: 7,
+        image: "Images/lunch (2).jpg",
+        name: "Lobster",
+        category: "Lunch",
+        description: "lorem ipsum dolor sit amet consectetur adipisicig elit. Dolar libero cum obcaecati deserunt excepturi amet.",
+    },
+    {
+        id: 8,
+        image: "Images/dinner (2).jpg",
+        name: "Light Lobster",
+        category: "Dinner",
+        description: "lorem ipsum dolor sit amet consectetur adipisicig elit. Dolar libero cum obcaecati deserunt excepturi amet.",
+    },
+];
+
+export default Menu;
